6845520a1ad0959271d9131f4072fe200aa5f8ea
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-08-23 11:38:44 -0500

    feat: :fire: Se quitaron funciones que no son necesarias

--- controllers/compraController.js
+++ controllers/compraController.js
@@ -1,29 +1,6 @@
 
 const Compra = require('../models/compra');
 
-exports.iniciarCompra = async (req, res) => {
-    let obj = new Compra();
-    try {
-        const { boleto, usuarioId, metodoPago, reserva } = req.body;
-        const result = await obj.iniciarCompra(boleto, usuarioId, metodoPago, reserva);
-        res.status(200).send(result);
-    } catch (error) {
-        console.error('Error al iniciar la compra:', error); // Registro detallado del error
-        res.status(500).send({ mensaje: 'Error al iniciar la compra', error: error.message });
-    }
-    obj.destructor()
-}
-
-
-
-exports.cancelarReserva = async (req, res) => {
-    let obj = new Compra();
-    const compraId = req.params.compraId;
-    const result = await obj.cancelarReserva(compraId);
-    res.status(200).send(result);
-    obj.destructor()
-}
-
 
 exports.actualizarReserva = async (req, res) => {
     let obj = new Compra();
--- routes/compraRoutes.js
+++ routes/compraRoutes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const compraController = require("../controllers/compraController");
 
-router.post('/compra', compraController.iniciarCompra);
-router.delete('/compra/:compraId', compraController.cancelarReserva);
 router.post('/reserva', compraController.actualizarReserva);
 
 module.exports = router;
